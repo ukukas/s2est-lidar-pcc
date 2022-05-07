a0038093a489a77a4d9111f4b00469182c458a21
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2022-05-07 16:28:45 -0400

    refactor spatial lag calcluation code

--- s2est-calculate-lag.R
+++ s2est-calculate-lag.R
@@ -1,4 +1,4 @@
-librarian::shelf(magrittr, readr, dplyr, parallel, multidplyr)
+librarian::shelf(magrittr, readr, dplyr)
 
 read_table <- function(year, tile, res, minfrac) {
     base::sprintf('grdtbl_%i_%s_%im.csv', year, tile, res) %>%
@@ -7,26 +7,18 @@ read_table <- function(year, tile, res, minfrac) {
         return()
 }
 
-get_lag <- function(table, dist, refP, refL){
-    table %>%
-        dplyr::filter(dplyr::between(P, refP-dist, refP+dist),
-                  dplyr::between(L, refL-dist, refL+dist),
-                  !(P==refP & L==refL)) %>%
-        dplyr::pull(pcc) %>%
-        base::mean() %>%
-        return()
-}
-
-calculate_lag <- function(table, dist, cores, lagfunc) {
-    cl <- multidplyr::new_cluster(cores)
-    multidplyr::cluster_library(cl, c('magrittr', 'dplyr'))
-    multidplyr::cluster_copy(cl, c('lagfunc', 'table'))
-    table %>%
-        dplyr::rowwise() %>%
-        multidplyr::partition(cl) %>%
-        dplyr::mutate(lag = lagfunc(table, P, L)) %>%
-        dplyr::collect() %>%
-        dplyr::ungroup() %>%
+calculate_lag <- function(table) {
+    table %<>% dplyr::select(P, L, pcc)
+    dplyr::bind_rows(dplyr::mutate(table, P=P-1, L=L-1),
+                     dplyr::mutate(table, P=P-1),
+                     dplyr::mutate(table, P=P-1, L=L+1),
+                     dplyr::mutate(table, L=L-1),
+                     dplyr::mutate(table, L=L+1),
+                     dplyr::mutate(table, P=P+1, L=L-1),
+                     dplyr::mutate(table, P=P+1),
+                     dplyr::mutate(table, P=P+1, L=L+1)) %>%
+        dplyr::group_by(P, L) %>%
+        dplyr::summarise(lag = base::mean(pcc)) %>%
         return()
 }
 
@@ -35,10 +27,8 @@ main <- function() {
     tile <- '35VLF'
     res <- 20
     minfrac <- 0.1
-    dist <- 1
-    cores <- parallel::detectCores() - 1
     read_table(year, tile, res, minfrac) %>%
-        calculate_lag(dist, cores, get_lag) %>%
+        dplyr::inner_join(calculate_lag(.)) %>%
         readr::write_csv(
             base::sprintf('pcctbl_%i_%s_%im.csv', year, tile, res))
     return(TRUE)
